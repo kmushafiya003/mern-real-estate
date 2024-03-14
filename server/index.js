4e3014aafab8b403dc67a2b3e549d9307b14f08d
Mushafiya <kmushafiya003@gmail.com>
2024-03-14 13:21:39 +0530

    Connect to Database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.log(err);
+
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log("Server is running....");
-})
-
+app.listen(3000, () => {
+  console.log('Server is running....');
+});
