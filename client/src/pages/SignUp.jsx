868766d277d00e26947ad7dd57245c44875a4e27
Mushafiya <kmushafiya003@gmail.com>
2024-03-16 13:16:46 +0530

    Complete Sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,20 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+
+  const navigate = useNavigate();
+
+  const [formData , setFormData] = useState({});
+  const [error , setError] = useState("");
+  const [loading , setLoading] = useState(false);
+
+
+  const handleChange = (events) => {
+
+    setFormData({
+      ...formData ,
+      [events.target.id] : events.target.value,
+    })
+
+
+  }
+
+
+  const handleSubmit = async(events) => {
+    events.preventDefault();
+
+    try{
+
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }
+      );
+  
+      const data = await res.json();
+      console.log("Data: " , data);
+
+      if(data.success === false){
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+  
+      setLoading(false);
+      setError(null);
+
+      navigate("/sign-in")
+  
+     
+
+    }catch(err){
+        setLoading(false);
+        setError(err);
+    }
+
+  
+
+   
+
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto' >
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
 
-      <form className='flex flex-col gap-y-4'>
+      <form className='flex flex-col gap-y-4' onSubmit={handleSubmit}>
 
-        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username'/>
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email'/>
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password'/>
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
 
-        <button  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+        <button disabled={loading}  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{
+          loading ? "Loading..." : "Sign Up"
+        }</button>
 
       </form>
 
@@ -25,6 +90,9 @@ const SignUp = () => {
         </Link>
         
       </div>
+      {
+        error && <p className='text-red-500 mt-5'>{error}</p>
+      }
     </div>
   )
 }
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,21 +1,30 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
-  
+
   //fetch data from request
   const { username, email, password } = req.body;
+
+  //validation
+
+  if(!username || !email || !password){
+    console.log("All fields are required*");
+    return next(errorHandler(404 , "All fields are required*"));
+  }
 
   //hash the password
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
-
+ 
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
 
     res.status(201).json({
+      success: true,
       message: 'User created Successfully',
     });
 
