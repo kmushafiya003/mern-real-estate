f5591fb09375a52f39a0dd49eb3a15c1f9e2d4ae
Mushafiya <kmushafiya003@gmail.com>
2024-03-19 12:19:09 +0530

    Add Redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,99 +1,96 @@
-
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { signInStart } from '../redux/user/userSlice';
+import { signInSuccess } from '../redux/user/userSlice';
+import { signInFailure } from '../redux/user/userSlice';
 
 const Signin = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const [formData , setFormData] = useState({});
-  const [error , setError] = useState("");
-  const [loading , setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
 
+
+  const {loading , error , } = useSelector((state) => state.user);
 
   const handleChange = (events) => {
-
     setFormData({
-      ...formData ,
-      [events.target.id] : events.target.value,
-    })
+      ...formData,
+      [events.target.id]: events.target.value,
+    });
+  };
 
-
-  }
-
-
-  const handleSubmit = async(events) => {
+  const handleSubmit = async (events) => { 
     events.preventDefault();
 
-    try{
-
-      setLoading(true);
-      const res = await fetch('/api/auth/signin',
-      {
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
-          'Content-Type' : 'application/json',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
-      }
-      );
-  
-      const data = await res.json();
-      console.log("Data: " , data);
+      });
 
-      if(data.success === false){
-        setError(data.message);
-        setLoading(false);
+      const data = await res.json();
+      console.log('Data: ', data);
+
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
       }
-  
-      setLoading(false);
-      setError(null);
 
-      navigate("/")
-  
-     
+      dispatch(signInSuccess(data.rest));
 
-    }catch(err){
-        setLoading(false);
-        setError(err);
+      navigate('/');
+      
+    } catch (err) {
+     dispatch(signInFailure(err.message))
     }
-
-   
-
-  }
+  };
 
   return (
-    <div className='p-3 max-w-lg mx-auto' >
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
 
-      <form className='flex flex-col gap-y-4' onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
 
-        
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
 
-        <button disabled={loading}  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{
-          loading ? "Loading..." : "Sign In"
-        }</button>
-
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
       </form>
 
-      <div className='flex gap-x-2 mt-5'>
+      <div className="flex gap-x-2 mt-5">
         <p>Dont have an account?</p>
         <Link to="/sign-up">
-           <span className='text-blue-700'>Sign up</span>
+          <span className="text-blue-700">Sign up</span>
         </Link>
-        
       </div>
-      {
-        error && <p className='text-red-500 mt-5'>{error}</p>
-      }
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-
-export default Signin
+export default Signin;
